38d69bdf775b399d6f7b025aee3ae88f99269a52
ericvasil87 <eric.vasil87@gmail.com>
2026-06-19 07:59:29 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'contractorai-v2';
+const ASSETS = [
+  '/contractor-demo/',
+  '/contractor-demo/index.html',
+  '/contractor-demo/mason.html',
+  '/contractor-demo/residential.html',
+  '/contractor-demo/commercial.html',
+  '/contractor-demo/cad.html',
+  '/contractor-demo/contractor-tools.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
